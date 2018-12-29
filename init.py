83f8e5c71b0dca5f5d17e6bc4865c06c41d50eaf
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2018-12-29 12:36:58 -0800

    webbrowser,ctypes

--- init.py
+++ init.py
@@ -1,5 +1,6 @@
-import tkinter,json,pytesseract,urllib.request,difflib
+import tkinter,json,pytesseract,urllib.request,difflib,webbrowser
 from PIL import ImageGrab
 from PIL import Image
 from pynput import keyboard
 from functions import *
+import ctypes
