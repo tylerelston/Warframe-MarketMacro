6e1a1ea65a45f0ef7d7e11fcfee1624505f9ff16
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2018-12-29 11:52:05 -0800

    plat

--- functions.py
+++ functions.py
@@ -1,9 +1,17 @@
 from init import *
 
-def item_price(name):
+def item_ducats(name):
     name = name.replace(' ', '_').lower()
     url= urllib.request.urlopen('https://api.warframe.market/v1/items/'+name)
     string = url.read().decode('utf-8')
     json_obj = json.loads(string)
     ducats = json_obj['payload']['item']['items_in_set'][0]['ducats']
-    print(ducats)
+    return ducats
+
+def item_plat(name):
+    name = name.replace(' ', '_').lower()
+    url= urllib.request.urlopen('https://api.warframe.market/v1/items/'+name+'/statistics')
+    string = url.read().decode('utf-8')
+    json_obj = json.loads(string)
+    plat = json_obj['payload']['statistics_closed']['48hours'][0]['avg_price']
+    return plat
